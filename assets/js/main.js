8ebac666e81189716067e9d434850bdda836c90d
Nguyen Thanh Tung <tung19022003@gmail.com>
2025-10-31 00:44:04 +0700

    fix

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+const style = document.createElement('style');
+style.textContent = `
+    @keyframes ripple {
+        0% {
+            transform: scale(0);
+            opacity: 1;
+        }
+        100% {
+            transform: scale(1);
+            opacity: 0;
+        }
+    }
+`;
+document.head.appendChild(style);
